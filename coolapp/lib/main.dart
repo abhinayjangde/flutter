5e01b144726af1aa197a07ae7fe89d1dbfbea6d9
Abhinay Jangde <64852930+abhinayjangde@users.noreply.github.com>
2026-06-27 09:39:08 +0000

    hello world

--- coolapp/lib/main.dart
+++ coolapp/lib/main.dart
@@ -4,76 +4,32 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Hello")),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Container(
-                        height: 200,
-                        color: Colors.blue,
-                        margin: EdgeInsets.all(8.0),
-                      ),
-                      Container(
-                        height: 200,
-                        color: Colors.red,
-                        margin: EdgeInsets.all(8.0),
-                      ),
-                    ],
-                  ),
-                ),
+      title: "Demo App",
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: const DashboardScreen(),
+    );
+  }
+}
 
-                Container(
-                  height: 200,
-                  color: Colors.redAccent,
-                  margin: EdgeInsets.all(8.0),
-                ),
-                Container(
-                  height: 200,
-                  color: Colors.green,
-                  margin: EdgeInsets.all(8.0),
-                ),
-                Container(
-                  height: 200,
-                  color: Colors.orange,
-                  margin: EdgeInsets.all(8.0),
-                ),
-                Container(
-                  height: 200,
-                  color: Colors.pink,
-                  margin: EdgeInsets.all(8.0),
-                ),
-                Container(
-                  height: 200,
-                  color: Colors.black,
-                  margin: EdgeInsets.all(8.0),
-                ),
-                Container(
-                  height: 200,
-                  color: Colors.yellow,
-                  margin: EdgeInsets.all(8.0),
-                ),
-              ],
-            ),
-          ),
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("hello")),
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          color: Colors.amber,
+          child: Center(child: Text("Hello World")),
         ),
       ),
     );
